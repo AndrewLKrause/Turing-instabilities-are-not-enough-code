f76ee7377cbcd9e72634ade26ecd7df795e90d56
Mar5bar <7798410+Mar5bar@users.noreply.github.com>
2023-08-14 10:05:01 +0100

    evaluateRuns updated to be more informative

--- evaluateRuns.m
+++ evaluateRuns.m
@@ -1,6 +1,8 @@
 mask = logical(TuringUnstable);
+numNoPatterns = sum(Patterns <= 1e-5);
 numWhereTuringNotEnough = sum(Patterns(mask) <= 1e-5);
 
 disp(modelName)
-disp([num2str(NumRuns), ' runs. ', num2str(sum(mask)), ' were Turing unstable. ', 'Of those, ', num2str(numWhereTuringNotEnough), ' resulted in no patterns.'])
+disp([num2str(NumRuns), ' runs in total. Of those, ', num2str(numNoPatterns), ' ended up with no patterns.']);
+disp([num2str(sum(mask)), ' of the total were Turing unstable. ', 'Of those, ', num2str(numWhereTuringNotEnough), ' ended up with no patterns.'])
 disp(['So, Turing was not enough in ', num2str(100*numWhereTuringNotEnough/sum(mask),'%.2f'), '% of cases.'])
