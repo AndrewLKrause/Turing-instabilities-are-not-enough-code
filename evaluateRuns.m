821fd1d1969923241780580be4fdb5bde5e42ba2
Mar5bar <7798410+Mar5bar@users.noreply.github.com>
2023-08-15 10:03:31 +0100

    2D data added. NonlocalAdvection1D added. evaluateRuns now reports dim

--- evaluateRuns.m
+++ evaluateRuns.m
@@ -2,7 +2,7 @@ mask = logical(TuringUnstable);
 numNoPatterns = sum(Patterns <= 1e-5);
 numWhereTuringNotEnough = sum(Patterns(mask) <= 1e-5);
 
-disp(modelName)
+disp([modelName, ' in ', num2str(dims), 'D.'])
 disp([num2str(NumRuns), ' runs in total. Of those, ', num2str(numNoPatterns), ' ended up with no patterns.']);
 disp([num2str(sum(mask)), ' of the total were Turing unstable. ', 'Of those, ', num2str(numWhereTuringNotEnough), ' ended up with no patterns.'])
 disp(['So, Turing was not enough in ', num2str(100*numWhereTuringNotEnough/sum(mask),'%.2f'), '% of cases.'])
